c5f4fddeaad484c9ff71bf2817ae1f8dadb10b4f
Mmaphuti Kgomo <mmaphutikgomo6@icloud.com>
2026-07-17 16:13:21 +0200

    Hamburger menu partially completed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,28 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text } from '../Text/Text'
 import coffeeLogo from '../../assets/coffee-logo.png'
 
 export const Navbar = () => {
-    return (
-            <nav className="navbar-container">
-                <div className="navbar-left">
-                    <div id="logo-text">
-                        <Text variant="h2" style={{ margin: 0 }}>Flavored</Text>
-                        <img src={coffeeLogo} alt="Coffee Shop Logo" id='logo-image' />
-                    </div>
-                    <div id='slogan'>
-                        <Text variant="h3" style={{ margin: 0 }}>Wake up to something special.</Text>
-                    </div>
-                </div>
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-                <div className='links'>
-                    <a href="#">Home</a>
-                    <a href="#">Coffee Menu</a>
-                    <a href="#">About Us</a>
-                    <a href="#">Contact Us</a>
-                    <a href="#" id='coffee-shop'>Coffee Shop</a>
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <nav className="navbar-container">
+            <div className="navbar-left">
+                <div id="logo-text">
+                    <Text variant="h2" style={{ margin: 0 }}>Flavored</Text>
+                    <img src={coffeeLogo} alt="Coffee Shop Logo" id='logo-image' />
                 </div>
-            </nav>
+                <div id='slogan'>
+                    <Text variant="h3" style={{ margin: 0 }}>Wake up to something special.</Text>
+                </div>
+            </div>
+
+            <button className='hamburger-menu' onClick={toggleMenu}>
+                {isMenuOpen ? 'x' : '☰'}
+            </button>
+
+            {isMenuOpen && (
+                <div className='links'>
+                    <a href="#" onClick={toggleMenu}> Home</a>
+                    <a href="#" onClick={toggleMenu}>Coffee Menu</a>
+                    <a href="#" onClick={toggleMenu}>About Us</a>
+                    <a href="#" onClick={toggleMenu}>Contact Us</a>
+                    <a href="#" id='coffee-shop' onClick={toggleMenu}>Coffee Shop</a>
+                </div>
+            )}
+
+        </nav>
     )
 }
 
